994438d7bee88b9430b7f96d9abe9401b430af8b
Abel Kifletsion <fishabel8@gmail.com>
2023-06-20 17:00:50 -0500

    added backend project

---
+++ backend/api/src/test/java/edu/miu/cs425/flightbookingsystem/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
