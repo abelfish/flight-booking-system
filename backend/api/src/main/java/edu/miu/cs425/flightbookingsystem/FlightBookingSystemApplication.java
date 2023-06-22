b690c5d4622010b1fcab3d95d252739e18345009
Sami1 <wediwelday23@gmail.com>
2023-06-22 16:40:26 -0500

    User and Role functionalities added

--- backend/api/src/main/java/edu/miu/cs425/flightbookingsystem/FlightBookingSystemApplication.java
+++ backend/api/src/main/java/edu/miu/cs425/flightbookingsystem/FlightBookingSystemApplication.java
@@ -1,13 +1,18 @@
 package edu.miu.cs425.flightbookingsystem;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlightBookingSystemApplication {
+public class FlightBookingSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightBookingSystemApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
--- backend/api/src/main/java/edu/miu/cs425/flightbookingsystem/service/UserService.java
+++ backend/api/src/main/java/edu/miu/cs425/flightbookingsystem/service/UserService.java
@@ -1,16 +1,15 @@
-//package edu.miu.cs425.flightbookingsystem.service;
-//
-//import edu.miu.cs425.flightbookingsystem.model.User;
-//
-//import java.util.List;
-//
-//public interface UserService {
-//    User addUser(User newUser);
-//    User getUserById(Long id);
-//    User updateUserById(Long userId, User user);
-//    User updateUser(User user);
-//    void deleteUserById(Long userId);
-//
-//    List<User> getAllUsers();
-////    List<Object[]> generateReport();
-//}
+package edu.miu.cs425.flightbookingsystem.service;
+
+import edu.miu.cs425.flightbookingsystem.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User addUser(User user);
+    List<User> getAllUsers();
+    User getUserById(Long userId);
+    User updateUserById(Long userId, User user);
+    User updateUser(User user);
+    void deleteUserById(Long userId);
+}
